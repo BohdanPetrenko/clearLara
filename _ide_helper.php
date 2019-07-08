cf9ae875bbd31fd405fc1a4304974543fe8089d5
bohdan <petrenko.bohfdan@pdffiller.team>
2019-07-08 14:37:20 +0300

    clear laraProject added jira_filters migration and viewing

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.27 on 2019-07-05 13:42:57.
+ * Generated for Laravel 5.8.27 on 2019-07-08 06:56:12.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
---
+++ app/Http/Controllers/TemperatureController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+
+class TemperatureController extends Controller
+{
+    //
+
+    public function show()
+    {
+        $client = new Client(['base_uri' => 'https://testslate.atlassian.net/rest/api/3/']);
+
+        $response = $client->request(
+            'GET',
+            'filter/search', [
+            'headers' => [
+                'Authorization' => "Basic " . base64_encode("korets.dmytro@pdffiller.team:KbMHUmjBxH39KmytnYh350A6")
+            ]]);
+
+        $filters = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+        $filters = $filters['values'];
+
+        return view('filters', compact('filters'));
+    }
+
+    function print_arr($arr)
+    {
+
+    }
+}
