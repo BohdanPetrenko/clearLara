21191cf4a6a2f521b20e19ee609201536f221117
bohdan <petrenko.bohfdan@pdffiller.team>
2019-07-08 16:19:32 +0300

    added Model and was changed the name of migration

---
+++ app/JiraFilter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JiraFilter extends Model
+{
+    protected $table = 'jira_filters';
+
+    protected $fillable = ['filter_id', 'query', 'max_total_items'];
+}
