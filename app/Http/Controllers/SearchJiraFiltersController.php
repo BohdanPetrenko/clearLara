29969bf0f606333317144b414dd49787e352d629
bohdan <petrenko.bohfdan@pdffiller.team>
2019-07-15 09:38:13 +0300

    add realization by scheduling

--- app/Http/Controllers/SearchJiraFiltersController.php
+++ app/Http/Controllers/SearchJiraFiltersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\JiraFilter;
 use App\Request\JiraFilterRequest;
 use App\Services\JiraProvider;
-use Carbon\Carbon;
 
 class SearchJiraFiltersController extends Controller
 {
@@ -21,7 +20,6 @@ class SearchJiraFiltersController extends Controller
     public function store(JiraFilterRequest $request)
     {
         $base = new JiraFilter($request->all());
-        $base->last_run = Carbon::now();
         $base->save();
     }
 }
